17bf1a4570f722983807bcda6aa28b693c479bea
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-07-26 08:24:59 +0700

    Update make more loop on conditional_loop.php

--- conditional_loop.php
+++ conditional_loop.php
@@ -2,44 +2,50 @@
 
 echo "1.<br>\n";
 for ($baris = 0; $baris < 3; $baris++) {
-    echo "*****<br>\n";
+    for ($garis = 0; $garis < 4; $garis++) {
+        echo "*";
+    }
+    echo "<br>\n";
 }
 
 echo "2.<br>\n";
 for ($baris = 0; $baris < 2; $baris++) {
     if ($baris == 1) {
-        echo "**", "_", "**<br>\n";
+        echo "** **<br>\n";
+    } else {
+        echo "*****<br>\n";
     }
-    echo "*****<br>\n";
 }
 
 echo "3.<br>\n";
 for ($baris = 1; $baris <= 5; $baris++) {
     for ($garis = 1; $garis <= $baris; $garis++) {
-        echo "*\n";
+        echo "*";
     }
-    echo "<br>";
+    echo "<br>\n";
 }
 
 echo "4.<br>\n";
 for ($baris = 5; $baris > 0; $baris--) {
     for ($garis = 1; $garis <= $baris; $garis++) {
-        echo "*\n";
+        echo "*";
     }
-    echo "<br>";
+    echo "<br>\n";
 }
 
 echo "5.<br>\n";
 for ($baris = 1; $baris <= 5; $baris++) {
     if ($baris == 1) {
         echo "*<br>\n";
-    } elseif ($baris == 2) {
+    } elseif ($baris == 2 || $baris == 3) {
         echo "**<br>\n";
-    } elseif ($baris == 3) {
-        echo "* *<br>\n";
     } elseif ($baris == 4) {
-        echo "*__*<br>\n";
+        echo "* *<br>\n";
     } else {
-        echo "*****<br>\n";
+        for ($garis = 1; $garis <= 5; $garis++) {
+            echo "*";
+        }
+        echo "<br>\n";
     }
 }
+?>
