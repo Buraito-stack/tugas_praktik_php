edfc63a498d7ade44a2eb230ec6eb958166dcc4b
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-07-31 16:52:21 +0800

    Add files via upload

---
+++ OOP/classes/Person.php
@@ -0,0 +1,14 @@
+<?php
+
+class Person {
+    protected string $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
+
