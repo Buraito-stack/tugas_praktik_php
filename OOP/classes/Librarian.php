fae38cf5a7e29f8e90eef10ad77c0d797a1e4eb3
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-08-02 08:02:45 +0700

    Update Librarian.php

--- OOP/classes/Librarian.php
+++ OOP/classes/Librarian.php
@@ -1,35 +1,40 @@
 <?php
 require_once 'Person.php';
 
-class Librarian extends Person {
+class Librarian extends Person 
+{
     private string $employeeID;
 
-    public function __construct($name, $employeeID) {
+    public function __construct(
+        string $name, 
+        string $employeeID
+    ) {
         parent::__construct($name);
         $this->employeeID = $employeeID;
     }
 
-    public function addBook(Book $book, Library $library) {
+    public function addBook(Book $book, Library $library) 
+    {
         $library->addBook($book);
     }
 
-    public function removeBook(Book $book, Library $library) {
+    public function removeBook(Book $book, Library $library) 
+    {
         $books = $library->listAvailableBooks();
+
         foreach ($books as $existingBook) {
             if ($existingBook->getISBN() === $book->getISBN()) {
-
-                // If the book is found, remove it
                 $library->removeBook($book);
                 
-                return; // Exit after removing the book
+                return; 
             }
         }
-
-        // If the book was not found, throw an exception
+        
         throw new Exception("Book not found in the library.");
     }
 
-    public function getLibrarianInfo() {
+    public function getLibrarianInfo() 
+    {
         return [
             'name' => $this->name,
             'employeeID' => $this->employeeID,
