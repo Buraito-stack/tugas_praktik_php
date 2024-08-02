1b105b5a83d1ec5b5babda4c1c886dde82269dd3
Bryant <139320860+Buraito-stack@users.noreply.github.com>
2024-08-02 08:03:26 +0700

    Update Member.php

--- OOP/classes/Member.php
+++ OOP/classes/Member.php
@@ -2,7 +2,8 @@
 require_once 'Person.php';
 
 
-class Member extends Person {
+class Member extends Person 
+{
     private string $memberID;
     private $borrowedBooks = [];
 
@@ -11,36 +12,45 @@ class Member extends Person {
         $this->memberID = $memberID;
     }
 
-    public function borrowBook(Book $book) {
+    public function borrowBook(Book $book) 
+    {
         if (in_array($book, $this->borrowedBooks)) {
             throw new Exception("Book already borrowed.");
         }
+        
         $book->borrowBook();
         $this->borrowedBooks[] = $book;
     }
 
-    public function returnBook(Book $book) {
+    public function returnBook(Book $book) 
+    {
         $key = array_search($book, $this->borrowedBooks);
-        if ($key === false) {
+
+        if (!$key) {
             throw new Exception("Book not borrowed.");
         }
+
         $book->returnBook();
         unset($this->borrowedBooks[$key]);
     }
 
-    public function getMemberInfo() {
+    public function getMemberInfo() 
+    {
         $borrowedBooksInfo = [];
+
         foreach ($this->borrowedBooks as $book) {
             $borrowedBooksInfo[] = $book->getBookInfo();
         }
+
         return [
-            'name' => $this->name,
-            'memberID' => $this->memberID,
+            'name'          => $this->name,
+            'memberID'      => $this->memberID,
             'borrowedBooks' => $borrowedBooksInfo,
         ];
     }
 
-    public function getMemberID() {
+    public function getMemberID() 
+    {
         return $this->memberID;
     }
 }
